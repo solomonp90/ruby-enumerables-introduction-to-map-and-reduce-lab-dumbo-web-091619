3290d12fc29a68f54f370918dbd1ae2b88b2ad0f
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-30 00:46:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,11 +8,11 @@ base.map{|i|-i.abs}
 end
 
 
-map { |obj| block } → array click to toggle source
-map → an_enumerator
-Returns a new array with the results of running block once for every element in enum.
-
-If no block is given, an enumerator is returned instead.
-
-(1..4).map { |i| i*i }      #=> [1, 4, 9, 16]
-(1..4).collect { "cat"  }   #=> ["cat", "cat", "cat", "cat"]
+# map { |obj| block } → array click to toggle source
+# map → an_enumerator
+# Returns a new array with the results of running block once for every element in enum.
+#
+# If no block is given, an enumerator is returned instead.
+#
+# (1..4).map { |i| i*i }      #=> [1, 4, 9, 16]
+# (1..4).collect { "cat"  }   #=> ["cat", "cat", "cat", "cat"]

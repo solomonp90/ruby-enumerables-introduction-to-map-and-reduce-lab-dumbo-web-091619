9450b1fe604a99df9babc53cf61ea983cff7a5a8
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-30 00:37:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,5 +7,12 @@ array.map{||}
 end
 
 
+map { |obj| block } → array click to toggle source
+map → an_enumerator
+Returns a new array with the results of running block once for every element in enum.
+
+If no block is given, an enumerator is returned instead.
+
+
 # (1..4).map { |i| i*i }      #=> [1, 4, 9, 16]
 # (1..4).collect { "cat"  }   #=> ["cat", "cat", "cat", "cat"]

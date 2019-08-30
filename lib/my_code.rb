fa61abe5b5e81ce10ff8b36aeb137137fb360a9b
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-30 05:33:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -22,7 +22,27 @@ end
 def reduce_to_total(array,starting_point=0)
     array.reduce(:+)+starting_point
 end
-
+# Combines all elements of enum by applying a binary operation, specified by a block or a symbol that names a method or operator.
+#
+# The inject and reduce methods are aliases. There is no performance benefit to either.
+#
+# If you specify a block, then for each element in enum the block is passed an accumulator value (memo) and the element. If you specify a symbol instead, then each element in the collection will be passed to the named method of memo. In either case, the result becomes the new value for memo. At the end of the iteration, the final value of memo is the return value for the method.
+#
+# If you do not explicitly specify an initial value for memo, then the first element of collection is used as the initial value of memo.
+#
+# # Sum some numbers
+# (5..10).reduce(:+)                             #=> 45
+# # Same using a block and inject
+# (5..10).inject { |sum, n| sum + n }            #=> 45
+# # Multiply some numbers
+# (5..10).reduce(1, :*)                          #=> 151200
+# # Same using a block
+# (5..10).inject(1) { |product, n| product * n } #=> 151200
+# # find the longest word
+# longest = %w{ cat sheep bear }.inject do |memo, word|
+#    memo.length > word.length ? memo : word
+# end
+# longest                                        #=> "sheep"
 def reduce_to_all_true(array)
   array.all?
 end

54c51d2f72a8906291f9c8c9667ab60ead94c18e
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-30 05:34:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -59,7 +59,16 @@ end
 def reduce_to_any_true(array)
   array.any?
 end
-
+# Passes each element of the collection to the given block. The method returns true if the block ever returns a value other than false or nil. If the block is not given, Ruby adds an implicit block of { |obj| obj } that will cause any? to return true if at least one of the collection members is not false or nil.
+#
+# If instead a pattern is supplied, the method returns whether pattern === element for any collection member.
+#
+# %w[ant bear cat].any? { |word| word.length >= 3 } #=> true
+# %w[ant bear cat].any? { |word| word.length >= 4 } #=> true
+# %w[ant bear cat].any?(/d/)                        #=> false
+# [nil, true, 99].any?(Integer)                     #=> true
+# [nil, true, 99].any?                              #=> true
+# [].any?                                           #=> false
 
 
 

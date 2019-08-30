294257564b858581235938ddd8f42428593eed33
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-30 03:51:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,7 +15,7 @@ def map_to_double(array)
   end
 
    def reduce_to_total(array,starting_point)
-array.inject {|starting_point|  }
+array.inject {|e,|  }
 end
 
 (5..10).inject { |sum, n| sum + n }
